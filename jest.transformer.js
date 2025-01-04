6324f7176228e8bdf943ad7bfa6cf783a95a4d9d
santosh-metafic <santosh@metafic.co>
2025-01-05 03:18:38 +0530

    javascript code

--- jest.transformer.js
+++
@@ -1,7 +0,0 @@
-// jest.transformer.js
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.createTransformer({
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
-});
